805eef996b13182ea5f20fe4f2d3133e92b983c5
AngelJostar <angelrojas@ciencias.unam.mx>
2023-02-27 17:12:06 -0600

    prototipos

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// DATE
+
+console.log(Date());
+
+let fecha = new Date();
+console.log(fecha);
+// dia del mes
+console.log(fecha.getDate());
+// dia de la semana
+console.log(fecha.getDay());
+// dia del mes
+console.log(fecha.getMonth());
+
+console.log(fecha.getFullYearYear());
