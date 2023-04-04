ef61931f041a0eb96341f342d3398f496bf4c572
AngelJostar <angelrojas@ciencias.unam.mx>
2023-04-04 16:43:21 -0600

    iteradores

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-<<<<<<< HEAD
-// SET
+// ITERABLES % ITERATORS
 
-const set = new Set([1,2,3,4,5,6,7,true,false,false,{},{},"hola","HOLA"]);
-console.log(set);
-=======
->>>>>>> 5bfc6e993082715154ecbb5b6cdc7c460d9dc652
+const iterable = [1,2,3,4]; 
+// const iterable ="Hola mundo";
+// const iterable = new Set([1,2,33,3,4,5]);
+// const iterable = new Map(["nombre","jon"],["edad",35])
+
+
+
+// accedemos al iterador de nuestros iterable
+const iterador = iterable[Symbol.iterator]();
+
+console.log(iterable);
+console.log(iterador);
+console.log(iterador.next());
+
+let next = iterador.next();
+while(!next.done){
+    console.log(next.value);
+    next = iterador.next();
+}
+
