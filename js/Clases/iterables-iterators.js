88bf27d1dbb8ba091eea16554bb83dd9d72b894e
AngelJostar <angelrojas@ciencias.unam.mx>
2023-04-28 17:32:02 -0600

    cambios

---
+++ js/Clases/iterables-iterators.js
@@ -0,0 +1,23 @@
+// ITERABLES % ITERATORS
+
+const iterable = [1,2,3,4]; 
+// const iterable ="Hola mundo";
+// const iterable = new Set([1,2,33,3,4,5]);
+// const iterable = new Map(["nombre","jon"],["edad",35])
+
+
+
+// accedemos al iterador de nuestros iterable
+const iterador = iterable[Symbol.iterator]();
+
+console.log(iterable);
+console.log(iterador);
+console.log(iterador.next());
+
+let next = iterador.next();
+while(!next.done){
+    console.log(next.value);
+    next = iterador.next();
+}
+
+ 
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
-// ITERABLES % ITERATORS
-
-const iterable = [1,2,3,4]; 
-// const iterable ="Hola mundo";
-// const iterable = new Set([1,2,33,3,4,5]);
-// const iterable = new Map(["nombre","jon"],["edad",35])
-
-
-
-// accedemos al iterador de nuestros iterable
-const iterador = iterable[Symbol.iterator]();
-
-console.log(iterable);
-console.log(iterador);
-console.log(iterador.next());
-
-let next = iterador.next();
-while(!next.done){
-    console.log(next.value);
-    next = iterador.next();
+function* iterable (){
+    yield "hola";
+    console.log("hola mundo");;
+    yield "hola 2";
+    console.log("seguimos con más instucciones de nuestro codigo");
+    yield "hola 3";
+    yield "hola 4";
 }
 
+let iterador = iterable();
+// console.log(iterador.next());
+// console.log(iterador.next());
+// console.log(iterador.next());
+
+for(let y of iterador){
+    console.log(y);
+}
+const arr = [...iterable()];
+console.log(arr);
+
+function cuadrado(valor){
+    setTimeout(() => {
+        console.log({valor, resultado: valor * valor});
+    }, Math.random()*1000);
+
+    return {
+        
+    }
+}
