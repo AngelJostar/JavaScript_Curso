58a8a03fc6ec99f128b65dd98610ad94cedc276f
AngelJostar <angelrojas@ciencias.unam.mx>
2023-05-08 23:50:32 -0600

    proxis

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
-// POXIS
+// THIS
 
- const persona = {
-    nombre:"",
-    apellido:"",
-    edad:0
- }
-
-
- const manejador = {
-    set(obj,prop,valor){
-        obj[prop] = valor;
-    }
- }
-
- const jon = new Proxy(persona,manejador);
- jon.nombre = "Join";
- jon.apellido = "Mircha";
- jon.edad = 35;
-
- console.log(jon);
+   
